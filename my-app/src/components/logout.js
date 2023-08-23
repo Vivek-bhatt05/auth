a561e1e0b9c2f4eacf3c7d5d404cfce8604d99a2
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-08-23 15:10:04 +0530

    ff

--- my-app/src/components/logout.js
+++ my-app/src/components/logout.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import { GoogleLogout } from 'react-google-login'
+// import React from 'react'
+// import { GoogleLogout } from 'react-google-login'
 
-const clientId= "804950120639-0176109v4dhiji5qtr1hosi00oml8d5g.apps.googleusercontent.com"
+// const clientId= "804950120639-0176109v4dhiji5qtr1hosi00oml8d5g.apps.googleusercontent.com"
+
+// const Logout = () => {
+
+//     const onSuccess=()=>{
+//         console.log("Log out successfull")
+//     }
+//   return (
+//     <div>
+//       <GoogleLogout
+//         clientId={clientId}
+//         buttonText={'Logout'}
+//         onLogoutSuccess={onSuccess}
+//         />
+//     </div>
+//   )
+// }
+
+// export default Logout
+
+import React from 'react';
+
+// const clientId = "804950120639-0176109v4dhiji5qtr1hosi00oml8d5g.apps.googleusercontent.com";
 
 const Logout = () => {
+  const onSuccess = () => {
+    console.log("Logout successful");
+  };
 
-    const onSuccess=()=>{
-        console.log("Log out successfull")
+  const handleLogout = () => {
+    const auth2 = window.gapi.auth2.getAuthInstance();
+    if (auth2 != null) {
+      auth2.signOut().then(onSuccess);
     }
-  return (
-    <div>
-      <GoogleLogout
-        clientId={clientId}
-        buttonText={'Logout'}
-        onLogoutSuccess={onSuccess}
-        />
-    </div>
-  )
-}
+  };
 
-export default Logout
+  return (
+    <div style={{marginTop:"30px"}}>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
+
+export default Logout;
